251ec2ab586f69f54e25025ba129fad1b872faf2
Emerson F. Talarico <emersonft@hotmail.com>
2021-01-09 15:40:39 -0300

    Finish front-web

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -1,4 +1,4 @@
-import { type } from "os";
+
 
 export type Product = {
     id: number;
@@ -8,9 +8,17 @@ export type Product = {
     imageUri: string;
 }
 
-export type OrderLocationdata = {
+export type OrderLocationData = {
     latitude: number;
     longitude: number;
     address: string;
 }
+
+type ProductId = {
+    id:number;
+}
+
+export type OrderPayload = {
+    products: ProductId[];
+} & OrderLocationData;
 
